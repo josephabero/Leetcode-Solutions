4eca1cef0637e2d403892fce2e5c4cc8de63d349
Joseph Abero <joseph.abero@yahoo.com>
2024-01-30 20:33:46 -0800

    Add analysis and solution of Move Zeroes

---
+++ top_100/12_move_zeroes/solution.py
@@ -0,0 +1,16 @@
+"""
+Move Zeroes
+https://leetcode.com/problems/move-zeroes/
+"""
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0 and nums[left] == 0:
+                nums[right], nums[left] = nums[left], nums[right]
+
+            if nums[left] != 0:
+                left += 1
